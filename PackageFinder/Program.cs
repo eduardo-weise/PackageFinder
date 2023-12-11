5e5b1b6e56ae2fea3604ad1a9047f4e29d6ca0b2
Eduardo Weise <eduardo.weise@ndd.tech>
2023-12-11 16:02:18 -0300

    refactor: melhora na estrutura do código e remoção de propriedade e métodos não utilizados

--- PackageFinder/Program.cs
+++ PackageFinder/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Serialization;
+
+namespace PackageFinder;
 
 class Program
 {
@@ -8,6 +11,7 @@ class Program
     {
         PropertyNameCaseInsensitive = true
     };
+
 
     static async Task Main(string[] args)
     {
@@ -19,96 +23,110 @@ class Program
             return;
         }
 
-        var packages = await File.ReadAllLinesAsync(filePath);
+        string[] packages = await File.ReadAllLinesAsync(filePath);
 
         foreach (string package in packages)
         {
-            var packageInfo = await GetPackageInfo(package);
-            var vulnerabilities = await GetVulnerabilities(package);
-
+            CatalogEntry? packageInfo = await GetPackageInfo(package);
             Console.WriteLine($"Package: {package}");
-            
-            foreach (var property in typeof(CatalogEntry).GetProperties())
+
+            foreach (PropertyInfo property in typeof(CatalogEntry).GetProperties())
             {
-                var value = property.GetValue(packageInfo);
-                Console.WriteLine($"{property.Name}: {value}");
+                object? value = property.GetValue(packageInfo);
+                Console.WriteLine($"{property.Name}: {value ?? "N/A"}");
             }
 
+            List<Vulnerability>? vulnerabilities = await GetVulnerabilities(package);
             Console.WriteLine($"Vulnerabilities: {vulnerabilities?.Count ?? 0}");
 
             if (vulnerabilities is not null)
             {
                 foreach (Vulnerability vulnerability in vulnerabilities)
                 {
-                    Console.WriteLine($"- {vulnerability.Title}");
-                    Console.WriteLine($"  Description: {vulnerability.Description}");
-                    Console.WriteLine($"  Reference: {vulnerability.Reference}");
+                    Console.WriteLine(vulnerability.ToString());
                 }
             }
             Console.WriteLine();
         }
     }
 
-    static async Task<CatalogEntry?> GetPackageInfo(string package)
+    private static async Task<CatalogEntry?> GetPackageInfo(string package)
     {
-        var packageId = package.Split('@').First();
-        var packageVersion = package.Split('@').Last();
+        string packageId = package.Split('@').First();
+        string packageVersion = package.Split('@').Last();
 
-        var url = $"https://api.nuget.org/v3/registration5-semver1/{packageId.ToLower()}/{packageVersion}.json";
-        var responseApi = await httpClient.GetAsync(url);
-        var packageInfoResponse = await responseApi.Content.ReadAsStringAsync();
-        var packageInfo = JsonSerializer.Deserialize<PackageInfo>(packageInfoResponse, jsonOptions);
+        string url = $"https://api.nuget.org/v3/registration5-semver1/{packageId.ToLower()}/{packageVersion}.json";
 
-        var responseCatalogEntry = await httpClient.GetAsync(packageInfo!.CatalogEntryUrl);
-        var catalogEntryString = await responseCatalogEntry.Content.ReadAsStringAsync();
-        var root = JsonSerializer.Deserialize<CatalogEntry>(catalogEntryString, jsonOptions);
+        try
+        {
+            HttpResponseMessage responseApi = await httpClient.GetAsync(url);
+            responseApi.EnsureSuccessStatusCode();
+            PackageInfo? packageInfo = await DeserializeJsonAsync<PackageInfo>(responseApi);
 
-        return root;
+            if (packageInfo is not null)
+            {
+                HttpResponseMessage catalogEntryApi = await httpClient.GetAsync(packageInfo!.CatalogEntryUrl);
+                catalogEntryApi.EnsureSuccessStatusCode();
+                CatalogEntry? catalogEntry = await DeserializeJsonAsync<CatalogEntry>(catalogEntryApi);
+
+                return catalogEntry;
+            }
+
+            return null;
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Ocorreu um erro ao fazer a solicitação HTTP: {ex.Message}");
+            return null;
+        }
     }
 
-    static async Task<List<Vulnerability>?> GetVulnerabilities(string package)
+    private static async Task<List<Vulnerability>?> GetVulnerabilities(string package)
     {
-        var url = $"https://ossindex.sonatype.org/api/v3/component-report/{package}";
+        string url = $"https://ossindex.sonatype.org/api/v3/component-report/{package}";
         HttpResponseMessage response = await httpClient.GetAsync(url);
 
-        var json = await response.Content.ReadAsStringAsync();
-        ComponentReport? report = JsonSerializer.Deserialize<ComponentReport>(json, new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        });
+        string json = await response.Content.ReadAsStringAsync();
+        ComponentReport? report = JsonSerializer.Deserialize<ComponentReport>(json, jsonOptions);
 
         return report?.Vulnerabilities;
+    }
+
+    private static async Task<T?> DeserializeJsonAsync<T>(HttpResponseMessage response)
+    {
+        string json = await response.Content.ReadAsStringAsync();
+
+        if (json is null)
+            return default;
+
+        return JsonSerializer.Deserialize<T>(json, jsonOptions);
     }
 }
 
 public class PackageInfo
 {
     [JsonPropertyName("catalogEntry")]
-    public string CatalogEntryUrl { get; set; }
+    public string? CatalogEntryUrl { get; set; }
 }
 
 public class ComponentReport
 {
-    [JsonPropertyName("vulnerabilities")]
     public List<Vulnerability>? Vulnerabilities { get; set; }
 }
 
 public class Vulnerability
 {
-    [JsonPropertyName("title")]
     public string? Title { get; set; }
 
-    [JsonPropertyName("description")]
     public string? Description { get; set; }
 
-    [JsonPropertyName("reference")]
     public string? Reference { get; set; }
 
     public override string ToString()
     {
-        return $"Title: {Title ?? "N/A"}\n" +
-           $"Description: {Description ?? "N/A"}\n" +
-           $"Reference: {Reference ?? "N/A"}\n";
+        return $":\n - Title: {Title ?? "N/A"}" +
+           $"\n - Description: {Description ?? "N/A"}" +
+           $"\n - Reference: {Reference ?? "N/A"}";
     }
 }
 
@@ -121,9 +139,9 @@ public class Deprecation
 
     public override string ToString()
     {
-        return $":\n    Id: {Id}" +
-           $"\n Message: {Message ?? "N/A"}" +
-           $"\n Reasons: {(Reasons is not null ? string.Join(", ", Reasons) : "N/A")}";
+        return $":\n - Id: {Id}" +
+           $"\n - Message: {Message ?? "N/A"}" +
+           $"\n - Reasons: {(Reasons is not null ? string.Join(", ", Reasons) : "N/A")}";
     }
 }
 
@@ -134,47 +152,8 @@ public class CatalogEntry
     public string? Authors { get; set; }
     public string? Copyright { get; set; }
     public DateTime? Created { get; set; }
-    public Deprecation? Deprecation { get; set; }
-    public string? Description { get; set; }
-    public string? IconUrl { get; set; }
-    public bool? IsPrerelease { get; set; }
+    public string? Version { get; set; }
     public DateTime? LastEdited { get; set; }
     public string? LicenseUrl { get; set; }
-    public bool? Listed { get; set; }
-    public string? PackageHash { get; set; }
-    public string? PackageHashAlgorithm { get; set; }
-    public int? PackageSize { get; set; }
-    public string? ProjectUrl { get; set; }
-    public DateTime? Published { get; set; }
-    public string? Repository { get; set; }
-    public bool? RequireLicenseAcceptance { get; set; }
-    public string? Serviceable { get; set; }
-    public string? VerbatimVersion { get; set; }
-    public string? Version { get; set; }
-
-    public override string ToString()
-    {
-        return $"\n{nameof(CatalogEntry)}:" +
-               $"\n  {nameof(Id)}: {Id}" +
-               $"\n  {nameof(Authors)}: {Authors}" +
-               $"\n  {nameof(Copyright)}: {Copyright}" +
-               $"\n  {nameof(Created)}: {Created}" +
-               $"\n  {nameof(Deprecation)}: {Deprecation}" +
-               $"\n  {nameof(Description)}: {Description}" +
-               $"\n  {nameof(IconUrl)}: {IconUrl}" +
-               $"\n  {nameof(IsPrerelease)}: {IsPrerelease}" +
-               $"\n  {nameof(LastEdited)}: {LastEdited}" +
-               $"\n  {nameof(LicenseUrl)}: {LicenseUrl}" +
-               $"\n  {nameof(Listed)}: {Listed}" +
-               $"\n  {nameof(PackageHash)}: {PackageHash}" +
-               $"\n  {nameof(PackageHashAlgorithm)}: {PackageHashAlgorithm}" +
-               $"\n  {nameof(PackageSize)}: {PackageSize}" +
-               $"\n  {nameof(ProjectUrl)}: {ProjectUrl}" +
-               $"\n  {nameof(Published)}: {Published}" +
-               $"\n  {nameof(Repository)}: {Repository}" +
-               $"\n  {nameof(RequireLicenseAcceptance)}: {RequireLicenseAcceptance}" +
-               $"\n  {nameof(Serviceable)}: {Serviceable}" +
-               $"\n  {nameof(VerbatimVersion)}: {VerbatimVersion}" +
-               $"\n  {nameof(Version)}: {Version}";
-    }
+    public Deprecation? Deprecation { get; set; }
 }
